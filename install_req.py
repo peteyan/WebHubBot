bce69f6f06237f563ec587d9069ff4335ce7aab2
Pete <pete.yan@aliyun.com>
2018-12-18 18:56:37 +0800

    env for macos

---
+++ install_req.py
@@ -0,0 +1,37 @@
+# !/usr/bin/python
+# -*- coding:utf-8 -*-
+# Author: Pete Yan <pete.yan@aliyun.com>
+# Date  : 2018/7/27
+
+import os
+
+if __name__ == '__main__':
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple attrs --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple Automat --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple certifi --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple cffi --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple chardet --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple constantly --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple cryptography --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple cssselect --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple enum34 --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple hyperlink --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple idna --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple incremental --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple ipaddress --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple lxml --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple parsel --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pyasn1-modules --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pyasn1 --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pycparser --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple PyDispatcher --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pyopenssl --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple queuelib --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple requests --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple Scrapy --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple service-identity --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple Twisted --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple urllib3 --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple w3lib --user')
+    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple zope.interface --user')
+
